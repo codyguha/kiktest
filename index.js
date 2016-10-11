d906da0bfb84e97273669e9bde9ebefc393dbe6d
codyguha <aalo.guha@gmail.com>
2016-10-11 12:59:54 -0700

    surveybot

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
 'use strict';
-var _ = require( "underscore" );
-var mongodb = require('mongodb');
+
+let mongodb = require('mongodb');
 let util = require('util');
 let http = require('http');
-let Bot  = require('@kikinteractive/kik');
+let Bot = require('@kikinteractive/kik');
 
 var uri = 'mongodb://heroku_w9p37vp9:3huc3hgd51uauubr921v8ctb4l@ds053216.mlab.com:53216/heroku_w9p37vp9';
 
-var seedData = [
-  {
-    user: "cody",
-    hungry: 'YES!',
-    relationship: 'You Light Up My Life',
-    detail: "10",
-    mood: "",
-    preference: ""
-  }
-]
+var seedData = [{
+  user: "",
+  hungry: "",
+  relationship: "",
+  detail: "",
+  mood: "",
+  preference: ""
+}]
 
 mongodb.MongoClient.connect(uri, function(err, db) {
-  
-  if(err) throw err;
+
+  if (err) throw err;
 
   var results = db.collection('results');
 
   results.insert(seedData, function(err, result) {
-    
-    		if(err) throw err;
-		});
-// Configure the bot API endpoint, details for your bot
-let bot = new Bot({
-    username: 'surveychicken',
-    apiKey: 'ea2ff779-c0cb-4ee8-9e22-377629a80c5d',
+
+    if (err) throw err;
+  });
+
+  let bot = new Bot({
+    username: 'vclabs_surveybot',
+    apiKey: 'd117253a-66ec-4fbb-a1d5-5b07089d153f',
     baseUrl: 'https://infinite-atoll-38383.herokuapp.com/'
-});
+  });
 
-bot.updateBotConfiguration();
+  bot.updateBotConfiguration();
 
-
-bot.onTextMessage(/Hi$/i, (incoming, next) => {
+  bot.onTextMessage(/Hi$/i, (incoming, next) => {
     bot.getUserProfile(incoming.from)
       .then((user) => {
-        const message = Bot.Message.text(`Hey ${user.firstName}! Do you have a moment to answer some questions about fried chicken ?`)
-          .addTextResponse(`Yes, I have time`)
+        const message = Bot.Message.text(`Hey ${user.firstName}! Please choose a survey to complete.`)
+          .addTextResponse(`Chicken`)
+          .addTextResponse(`Angus Reid Poll`)
+          .addTextResponse(`Another Survey`)
+          .addTextResponse(`Another Poll`)
           .addTextResponse(`No thanks`)
         incoming.reply(message)
       });
   });
+  
 
   bot.onStartChattingMessage((incoming) => {
     bot.getUserProfile(incoming.from)
       .then((user) => {
-        const message = Bot.Message.text(`Hey ${user.firstName}! Do you have a moment to answer some questions about fried chicken ?`)
-			.addTextResponse(`Yes, I have time`)
+        const message = Bot.Message.text(`Hey ${user.firstName}! I am the VC Labs survey bot.  You can complete surveys with me. Please choose a survey to complete.`)
+          .addTextResponse(`Chicken`)
+          .addTextResponse(`Angus Reid Poll`)
+          .addTextResponse(`Another Survey`)
+          .addTextResponse(`Another Poll`)
           .addTextResponse(`No thanks`)
         incoming.reply(message)
       });
@@ -66,21 +70,27 @@ bot.onTextMessage(/Hi$/i, (incoming, next) => {
       });
   });
 
- bot.onTextMessage(/no, I ate|YES!$/i, (incoming, next) => {
+  bot.onTextMessage(/no, I ate|YES!$/i, (incoming, next) => {
     bot.getUserProfile(incoming.from)
       .then((user) => {
-        const message = Bot.Message.text(`Thanks for your time. Say "hi" to take the survey again`)
+        const message = Bot.Message.text(`Thanks for your time. Say "hi" to take another survey.`)
         const hifive = Bot.Message.video(`http://media.giphy.com/media/uXNYDeQ20XWSs/giphy.gif`)
           .setAttributionName(' ')
           .setLoop(true)
           .setAutoplay(true)
           .setAttributionIcon('http://icons.iconarchive.com/icons/icons8/ios7/128/Animals-Chicken-icon.png')
-          results.update({user: user.username}, { $set: { hungry: incoming.body } })
-      return incoming.reply([hifive, message])
-    });
+        results.update({
+          user: user.username
+        }, {
+          $set: {
+            hungry: incoming.body
+          }
+        })
+        return incoming.reply([hifive, message])
+      });
   });
 
-  bot.onTextMessage(/Yes, I have time$/i, (incoming, next) => {
+  bot.onTextMessage(/Chicken$/i, (incoming, next) => {
     bot.getUserProfile(incoming.from)
       .then((user) => {
         const message = Bot.Message.text(`Awesome lets get started. What would you say your relationship is with fried chicken?`)
@@ -88,9 +98,11 @@ bot.onTextMessage(/Hi$/i, (incoming, next) => {
           .addTextResponse(`It's a guilty pleasure`)
           .addTextResponse(`Not really my thing`)
           .addTextResponse(`I’ll die before I eat fried chicken`)
-          results.insert({user: user.username})
-      return incoming.reply(message)
-    });
+        results.insert({
+          user: user.username
+        })
+        return incoming.reply(message)
+      });
   });
 
   bot.onTextMessage(/I love it$/i, (incoming, next) => {
@@ -101,9 +113,15 @@ bot.onTextMessage(/Hi$/i, (incoming, next) => {
           .addTextResponse(`KFC is my go to`)
           .addTextResponse(`Any fried chicken is good chicken`)
           .addTextResponse(`It's a secret and I’m not telling you`)
-      results.update({user: user.username}, { $set: { relationship: incoming.body } })
-      return incoming.reply(message);
-    });
+        results.update({
+          user: user.username
+        }, {
+          $set: {
+            relationship: incoming.body
+          }
+        })
+        return incoming.reply(message);
+      });
   });
 
   bot.onTextMessage(/It's a guilty pleasure$/i, (incoming, next) => {
@@ -113,9 +131,15 @@ bot.onTextMessage(/Hi$/i, (incoming, next) => {
           .addTextResponse(`After a night of hard partying`)
           .addTextResponse(`A treat if I’ve been eating good for while`)
           .addTextResponse(`It’s a personal matter`)
-      results.update({user: user.username}, { $set: { relationship: incoming.body } })
-      return incoming.reply(message)
-    });
+        results.update({
+          user: user.username
+        }, {
+          $set: {
+            relationship: incoming.body
+          }
+        })
+        return incoming.reply(message)
+      });
   });
 
   bot.onTextMessage(/Not really my thing|I’ll die before I eat fried chicken$/i, (incoming, next) => {
@@ -125,9 +149,15 @@ bot.onTextMessage(/Hi$/i, (incoming, next) => {
           .addTextResponse(`Chicken is God’s creature and shouldn’t be eaten`)
           .addTextResponse(`Fried food is gross`)
           .addTextResponse(`I’m not going to get into it`)
-      results.update({user: user.username}, { $set: { relationship: incoming.body } })
-      return incoming.reply(message)
-    });
+        results.update({
+          user: user.username
+        }, {
+          $set: {
+            relationship: incoming.body
+          }
+        })
+        return incoming.reply(message)
+      });
   });
 
   bot.onTextMessage(/^After a night of hard partying|I’m not going to get into it|Fried food is gross|Chicken is God’s creature and shouldn’t be eaten|A treat if I’ve been eating good for while|It’s a personal matter|I make it myself|KFC is my go to|Any fried chicken is good chicken|It's a secret and I’m not telling you$/i, (incoming, next) => {
@@ -138,9 +168,15 @@ bot.onTextMessage(/Hi$/i, (incoming, next) => {
           .addTextResponse(`😊`)
           .addTextResponse(`😞`)
           .addTextResponse(`😠`)
-      results.update({user: user.username}, { $set: { detail: incoming.body } })
-      return incoming.reply(message);
-    });  
+        results.update({
+          user: user.username
+        }, {
+          $set: {
+            detail: incoming.body
+          }
+        })
+        return incoming.reply(message);
+      });
   });
 
   bot.onTextMessage(/^😀|😊|😞|😠$/i, (incoming, next) => {
@@ -163,27 +199,35 @@ bot.onTextMessage(/Hi$/i, (incoming, next) => {
           .addTextResponse('Double Down')
           .addTextResponse('Fried Drumsticks')
           .addTextResponse('Chicken Nuggets')
-      results.update({user: user.username}, { $set: { mood: incoming.body } })
-      return incoming.reply([pic1, pic2, pic3, pic4, message]);
-    });
+        results.update({
+          user: user.username
+        }, {
+          $set: {
+            mood: incoming.body
+          }
+        })
+        return incoming.reply([pic1, pic2, pic3, pic4, message]);
+      });
   });
 
   bot.onTextMessage(/^Chicken Parmesan|Double Down|Fried Drumsticks|Chicken Nuggets$/i, (incoming, next) => {
     bot.getUserProfile(incoming.from)
       .then((user) => {
         const message = Bot.Message.text(`Have we made you hungry answering these questions?`)
-        .addTextResponse(`YES!`)
-        .addTextResponse(`no, I ate`)
-      results.update({user: user.username}, { $set: { preference: incoming.body } })
-      return incoming.reply(message)
-    });  
+          .addTextResponse(`YES!`)
+          .addTextResponse(`no, I ate`)
+        results.update({
+          user: user.username
+        }, {
+          $set: {
+            preference: incoming.body
+          }
+        })
+        return incoming.reply(message)
+      });
   });
 
-// Set up your server and start listening
-let server = http
+  let server = http
     .createServer(bot.incoming())
     .listen(process.env.PORT || 8080);
 });
-
-
-
