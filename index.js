585145f8b7d39331909ecc4ffcc22d38ef124b4f
codyguha <aalo.guha@gmail.com>
2016-10-12 15:29:46 -0700

    survey chicken link

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
     bot.getUserProfile(incoming.from)
       .then((user) => {
         const message = Bot.Message.text(`Hello again ${user.firstName}! Please choose a survey to complete.`)
-          .addTextResponse(`Talk to our other bot @surveychicken about chicken`)
+          .addTextResponse(`@surveychicken Chicken Survey`)
           .addTextResponse(`Canadian Values Index`)
           .addTextResponse(`No thanks`)
         incoming.reply(message)
@@ -67,7 +67,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
     bot.getUserProfile(incoming.from)
       .then((user) => {
         const message = Bot.Message.text(`Hey ${user.firstName}! I am the VC Labs survey bot.  You can complete surveys with me. Please choose a survey to begin.`)
-          .addTextResponse(`Talk to our other bot @surveychicken about chicken`)
+          .addTextResponse(`@surveychicken Chicken Survey`)
           .addTextResponse(`Canadian Values Index`)
           .addTextResponse(`No thanks`)
         incoming.reply(message)
@@ -80,135 +80,11 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
     bot.getUserProfile(incoming.from)
       .then((user) => {
         const message = Bot.Message.text(`Hey ${user.firstName}! I am the VC Labs survey bot.  You can complete surveys with me. Please choose a survey to begin.`)
-          .addTextResponse(`Talk to our other bot @surveychicken about chicken`)
+          .addTextResponse(`@surveychicken Chicken Survey`)
           .addTextResponse(`Canadian Values Index`)
           .addTextResponse(`No thanks`)
         incoming.reply(message)
         results.insert({user: user.username})
-      });
-  });
-
-  
-
-  bot.onTextMessage(/I love it$/i, (incoming, next) => {
-    bot.getUserProfile(incoming.from)
-      .then((user) => {
-        const message = Bot.Message.text(`What is your favourite way to eat fried chicken?`)
-          .addTextResponse(`I make it myself`)
-          .addTextResponse(`KFC is my go to`)
-          .addTextResponse(`Any fried chicken is good chicken`)
-          .addTextResponse(`It's a secret and I’m not telling you`)
-        results.update({
-          user: user.username
-        }, {
-          $set: { 
-            "chicken_survey.relationship": incoming.body
-          }
-        })
-        return incoming.reply(message);
-      });
-  });
-
-  bot.onTextMessage(/It's a guilty pleasure$/i, (incoming, next) => {
-    bot.getUserProfile(incoming.from)
-      .then((user) => {
-        const message = Bot.Message.text(`Guilty pleasure you say, tell me more.`)
-          .addTextResponse(`After a night of hard partying`)
-          .addTextResponse(`A treat if I’ve been eating good for while`)
-          .addTextResponse(`It’s a personal matter`)
-        results.update({
-          user: user.username
-        }, {
-          $set: {
-            "chicken_survey.relationship": incoming.body
-          }
-        })
-        return incoming.reply(message)
-      });
-  });
-
-  bot.onTextMessage(/Not really my thing|I’ll die before I eat fried chicken$/i, (incoming, next) => {
-    bot.getUserProfile(incoming.from)
-      .then((user) => {
-        const message = Bot.Message.text(`So your not a fan eh? Tell us more.`)
-          .addTextResponse(`Chicken is God’s creature and shouldn’t be eaten`)
-          .addTextResponse(`Fried food is gross`)
-          .addTextResponse(`I’m not going to get into it`)
-        results.update({
-          user: user.username
-        }, {
-          $set: {
-            "chicken_survey.relationship": incoming.body
-          }
-        })
-        return incoming.reply(message)
-      });
-  });
-
-  bot.onTextMessage(/^After a night of hard partying|I’m not going to get into it|Fried food is gross|Chicken is God’s creature and shouldn’t be eaten|A treat if I’ve been eating good for while|It’s a personal matter|I make it myself|KFC is my go to|Any fried chicken is good chicken|It's a secret and I’m not telling you$/i, (incoming, next) => {
-    bot.getUserProfile(incoming.from)
-      .then((user) => {
-        const message = Bot.Message.text(`What is your current mood?`)
-          .addTextResponse(`😀`)
-          .addTextResponse(`😊`)
-          .addTextResponse(`😞`)
-          .addTextResponse(`😠`)
-        results.update({
-          user: user.username
-        }, {
-          $set: {
-            "chicken_survey.detail": incoming.body
-          }
-        })
-        return incoming.reply(message);
-      });
-  });
-
-  bot.onTextMessage(/^😀|😊|😞|😠$/i, (incoming, next) => {
-    bot.getUserProfile(incoming.from)
-      .then((user) => {
-        const pic1 = Bot.Message.picture(`http://fiber-international.com/wp-content/uploads/2015/04/800x600-chicken.jpg`)
-          .setAttributionName('Chicken Parmesan')
-          .setAttributionIcon('http://icons.iconarchive.com/icons/icons8/ios7/128/Animals-Chicken-icon.png')
-        const pic2 = Bot.Message.picture(`http://assets.bwbx.io/images/ieMg5BCeWkWU/v1/-1x-1.jpg`)
-          .setAttributionName('Double Down')
-          .setAttributionIcon('http://icons.iconarchive.com/icons/icons8/ios7/128/Animals-Chicken-icon.png')
-        const pic3 = Bot.Message.picture(`https://i.ytimg.com/vi/G8hbFO-r2nQ/maxresdefault.jpg`)
-          .setAttributionName('Fried Drumsticks')
-          .setAttributionIcon('http://icons.iconarchive.com/icons/icons8/ios7/128/Animals-Chicken-icon.png')
-        const pic4 = Bot.Message.picture(`http://www.urbanmommies.com/wp-content/uploads/McDonalds-Chicken-Nuggets.jpg`)
-          .setAttributionName('Chicken Nuggets')
-          .setAttributionIcon('http://icons.iconarchive.com/icons/icons8/ios7/128/Animals-Chicken-icon.png')
-        const message = Bot.Message.text(`Which of these would you like to be eating right now?`)
-          .addTextResponse('Chicken Parmesan')
-          .addTextResponse('Double Down')
-          .addTextResponse('Fried Drumsticks')
-          .addTextResponse('Chicken Nuggets')
-        results.update({
-          user: user.username
-        }, {
-          $set: {
-            "chicken_survey.mood": incoming.body
-          }
-        })
-        return incoming.reply([pic1, pic2, pic3, pic4, message]);
-      });
-  });
-
-  bot.onTextMessage(/^Chicken Parmesan|Double Down|Fried Drumsticks|Chicken Nuggets$/i, (incoming, next) => {
-    bot.getUserProfile(incoming.from)
-      .then((user) => {
-        const message = Bot.Message.text(`Have we made you hungry answering these questions?`)
-          .addTextResponse(`YES!`)
-          .addTextResponse(`no, I ate`)
-        results.update({
-          user: user.username
-        }, {
-          $set: {
-            "chicken_survey.preference": incoming.body
-          }
-        })
-        return incoming.reply(message)
       });
   });
 
@@ -218,26 +94,6 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
         const message = Bot.Message.text(`ok ${user.firstName}! say "Hi" agian sometime `)
         incoming.reply(message)
       });
-  });
-
- bot.onTextMessage(/no, I ate|YES!$/i, (incoming, next) => {
-    bot.getUserProfile(incoming.from)
-      .then((user) => {
-        const message = Bot.Message.text(`Thanks for your time. Say "hi" to take another survey.`)
-        const hifive = Bot.Message.video(`http://media.giphy.com/media/uXNYDeQ20XWSs/giphy.gif`)
-          .setAttributionName(' ')
-          .setLoop(true)
-          .setAutoplay(true)
-          .setAttributionIcon('http://icons.iconarchive.com/icons/icons8/ios7/128/Animals-Chicken-icon.png')
-        results.update({
-          user: user.username
-        }, {
-          $set: {
-            "chicken_survey.hungry": incoming.body
-          }
-        })
-      return incoming.reply([hifive, message])
-    });
   });
 
   //// CANADIAN VALUES SURVEY
